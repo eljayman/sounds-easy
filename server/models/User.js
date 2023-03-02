64c0313e779394b035070c0c75ffc6f3e74bf57e
eljayman <lelandoj@gmail.com>
2023-03-02 15:23:11 -0700

    made soundboard model, updated user

--- server/models/User.js
+++ server/models/User.js
@@ -1,6 +1,9 @@
 const { Schema, model } = require('mongoose');
 const bcrypt = require('bcrypt');
+const Soundboard = require('./Soundboard');
+// require Soundboard model for reference
 
+// user schema defines shape of object to be stored
 const userSchema = new Schema({
   username: {
     type: String,
@@ -12,6 +15,7 @@ const userSchema = new Schema({
     type: String,
     required: true,
     unique: true,
+    // validate email address
     match: [
         /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/,
         'Please use a valid email address',
@@ -22,6 +26,7 @@ const userSchema = new Schema({
     required: true,
     minlength: 5,
   },
+  // array of soundboards for the user
   soundboards: [
     {
       type: Schema.Types.ObjectId,
@@ -30,6 +35,7 @@ const userSchema = new Schema({
   ],
 });
 
+// middleware to hash password any time the user is created or updated
 userSchema.pre('save', async function (next) {
   if (this.isNew || this.isModified('password')) {
     const saltRounds = 10;
@@ -39,10 +45,17 @@ userSchema.pre('save', async function (next) {
   next();
 });
 
+// middleware deletes all soundboards a user created when it is deleted
+userSchema.post('findOneAndDelete', function (user) {
+    Soundboard.deleteMany({ username: user.username }).exec();
+  });
+
+  // method on the schema to check the password
 userSchema.methods.isCorrectPassword = async function (password) {
   return bcrypt.compare(password, this.password);
 };
 
+// compile the schema into the model
 const User = model('User', userSchema);
 
 module.exports = User;
