f3d179198752c2cd713d9638b6d860d9cd087e3c
eljayman <lelandoj@gmail.com>
2023-03-02 15:36:59 -0700

    inital models set

---
+++ server/models/Sound.js
@@ -0,0 +1,22 @@
+const { Schema, model } = require('mongoose');
+
+// model is name for and location of sound
+const soundSchema = new Schema({
+    soundname: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true,
+    },
+    url: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true,
+    },
+})
+
+// compile schema into model
+const Sound = model('Sound', soundSchema);
+
+module.exports = Sound;
--- server/models/index.js
+++ server/models/index.js
@@ -1,4 +1,5 @@
 const User = require('./User');
 const Soundboard = require('./Soundboard');
+const Sound = require('./Sound');
 
-module.exports = {User};
+module.exports = {User, Soundboard, Sound};
