e80c41ce1bc86d0d869468c3b3499a102d1ffd36
Ethikry <113566829+Ethikry@users.noreply.github.com>
2023-03-06 23:44:58 -0700

    Added styles to Dashboard, Library, Landing, Sound, Soundboard. Added Header and Footer and added those components to component/App/App.js. Removed buttons from landing and put them in Header.

--- client/src/components/Landing/Landing.js
+++ client/src/components/Landing/Landing.js
@@ -3,23 +3,16 @@ import { Link } from 'react-router-dom';
 
 const Landing = () => {
   return (
-    <div>
-      <h1>Welcome to Sounds Easy!</h1>
+    <div className="grid grid-cols-5">
+      <div className="col-start-2 col-span-3 items-center p-4">
+      <h1 className="text-center text-4xl">Welcome to Sounds Easy!</h1>
+      </div>
+      <div className="col-start-2 col-span-3 items-center p-2">
       <p>
         Sounds Easy is a platform where you can listen to and add sounds to your
-        own personal soundboard library. Create a profile to get started, or
+        own personal soundboard library. View and listen to different sounds on our soundboard, and create an easy-to-access personal library for on-demand sounds! Create a profile to get started, or
         view our sound library.
       </p>
-      <div>
-        <Link to="/signup">
-          <button>Sign Up</button>
-        </Link>
-        <Link to="/login">
-          <button>Login</button>
-        </Link>
-        <Link to="/soundboard">
-          <button>Sound Library</button>
-        </Link>
       </div>
     </div>
   );
--- client/src/components/Soundboard/Soundboard.js
+++ client/src/components/Soundboard/Soundboard.js
@@ -9,19 +9,23 @@ function Soundboard(soundProps) {
   };
 
   return (
-    <div className="soundboard">
-      <h2>Soundboard</h2>
-      <ul style={{ listStyleType: 'none' }}>
-        {soundProps.sounds.map((sound) => (
-          <Sound
-            key={sound.id}
-            image={sound.image}
-            file={sound.file}
-            title={sound.title}
-            onClick={() => handleSoundClick(sound)} //NEEDS CHANGE
-          />
-        ))}
-      </ul>
+    <div className="soundboard grid grid-cols-5">
+      <div className="col-start-2 col-span-3 items-center p-4">
+        <h1 className="text-center text-4xl">Soundboard</h1>
+      </div>
+      <div className="col-start-2 col-span-3 items-center p-2">
+        <ul style={{ listStyleType: 'none' }}>
+          {soundProps.sounds.map((sound) => (
+            <Sound
+              key={sound.id}
+              image={sound.image}
+              file={sound.file}
+              title={sound.title}
+              onClick={() => handleSoundClick(sound)} //NEEDS CHANGE
+            />
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
