e53feec15d04ffeab5e2ae334ece761ae208cea2
Ethikry <113566829+Ethikry@users.noreply.github.com>
2023-03-07 21:06:55 -0700

    Added return to dashboard.js

--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -1,43 +1,28 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import { useQuery } from '@apollo/client';
+import { QUERY_MY_SOUNDS } from '../../utils/queries';
+import Sound from '../Sound';
 
-const Dashboard = ({ username }) => {
-  const [library, setLibrary] = useState([]);
-
-  // Use the useEffect hook to fetch the user's library
-  useEffect(() => {
-    const fetchLibrary = async () => {
-      try {
-      } catch (error) {
-        console.error(error);
-      }
-    };
-
-    fetchLibrary();
-  }, [username]); // Re-run when the username changes
-
-  // Delete sound from library and update state
-  const handleDelete = async (id) => {
-    try {
-    } catch (error) {
-      console.error(error);
-    }
-  };
+const Dashboard = () => {
+  const { loading, data } = useQuery(QUERY_MY_SOUNDS);
+  const mySounds = data?.sounds || [];
+  console.log(mySounds);
 
   return (
-    <div className="grid grid-cols-5">
+    <div className="soundboard grid grid-cols-5">
       <div className="col-start-2 col-span-3 items-center p-4">
-        <h1 className="text-center text-4xl">Welcome {username}!</h1>
-      </div>
-      <div className="col-start-2 col-span-3 items-center p-4">
-        <h2 className="text-center text-3xl">Your Sounds:</h2>
+        <h1 className="text-center text-4xl">Soundboard</h1>
       </div>
       <div className="col-start-2 col-span-3 items-center p-2">
-        <ul>
-          {library.map((sound) => (
-            <li key={sound.id}>
-              {sound.name}
-              <button onClick={() => handleDelete(sound.id)}>Delete</button>
-            </li>
+        <ul style={{ listStyleType: 'none' }}>
+          {mySounds.map((data) => (
+            <Sound
+              key={data._id}
+              // image={image}
+              url={data.url}
+              soundName={data.soundName}
+              //onClick={() => handleSoundClick} NEEDS CHANGE
+            />
           ))}
         </ul>
       </div>
