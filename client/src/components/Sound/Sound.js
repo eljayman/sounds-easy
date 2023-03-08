de3151b58b949b5ca1763ebcb49a5c2226e34731
Ethikry <113566829+Ethikry@users.noreply.github.com>
2023-03-07 21:03:48 -0700

    Added way to select sounds and disply to library

--- client/src/components/Sound/Sound.js
+++ client/src/components/Sound/Sound.js
@@ -1,16 +1,47 @@
 import React, { useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import { useMutation } from '@apollo/client';
+import {
+  ADD_SOUND_TO_BOARD,
+  REMOVE_SOUND_FROM_BOARD,
+} from '../../utils/mutations';
+import Auth from '../../utils/auth';
 
 function Sound({ _id, soundName, url }) {
   const [audio] = useState(new Audio(url));
+  const [addSound] = useMutation(ADD_SOUND_TO_BOARD);
+  const [removeSound] = useMutation(REMOVE_SOUND_FROM_BOARD);
+  const location = useLocation();
 
   const handleClick = () => {
     audio.play();
   };
 
   return (
-    <li  className="sound inline-block p-4" onClick={handleClick}>
+    <li className="sound inline-block p-4" onClick={handleClick}>
       {/* <img src={sounds.image} alt={sounds.soundName} width={'160px'} /> */}
       <p>{soundName}</p>
+      <div>
+        {Auth.loggedIn() ? (
+          <>
+            <div>
+              {location.pathname === '/soundboard' ? (
+                <div>
+                  <button onClick={addSound(_id)}>Add Sound to library</button>
+                </div>
+              ) : (
+                <div>
+                  <button onClick={removeSound(_id)}>
+                    Add Sound to library
+                  </button>
+                </div>
+              )}
+            </div>
+          </>
+        ) : (
+          <></>
+        )}
+      </div>
     </li>
   );
 }
