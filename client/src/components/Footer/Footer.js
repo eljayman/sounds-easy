32aa737784d6c766984d5c659e374ee07768cbad
Ethikry <113566829+Ethikry@users.noreply.github.com>
2023-03-08 02:29:32 -0700

    Changed login, added redirect to dashboard if succesful and alert error

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,7 +1,7 @@
 function Footer() {
   return (
-    <footer className="bg-gray-800 bottom-0 z-10">
-      <div className="container mx-auto flex flex-wrap p-2 flex-col md:flex-row items-center">
+    <footer className="bg-gray-800 fixed bottom-0 z-10">
+      <div className="container mx-auto flex flex-wrap p-2 flex-col md:flex-row items-center ">
         <p>©Sounds Easy Team 2023</p>
         <div className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700 flex flex-wrap items-center text-base justify-center">
           <a className="px-4" href="https://github.com/eljayman/sounds-easy">
