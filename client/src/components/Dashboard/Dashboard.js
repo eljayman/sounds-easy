57a4e3294181283b50d086a21a7a66dbf4379e07
Ethikry <113566829+Ethikry@users.noreply.github.com>
2023-03-09 06:41:30 -0700

    Bugfix login redirect to dashboard

--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Navigate } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 import { QUERY_ME, QUERY_MY_SOUNDS } from '../../utils/queries';
 import Sound from '../Sound';
@@ -14,29 +15,41 @@ const Dashboard = () => {
   const { loading, data: soundData } = useQuery(QUERY_MY_SOUNDS);
   const mySounds = soundData?.mySounds || [];
 
-  return (
-    <div className="soundboard grid grid-cols-5">
-      <div className="col-start-2 col-span-3 items-center p-4">
-        <h1 className="text-center text-4xl">{me.username}'s Dashboard</h1>
+  const [authenticated, setAuthenticated] = useState(null);
+  useEffect(() => {
+    const loggedInUser = localStorage.getItem('id_token');
+    if (loggedInUser) {
+      setAuthenticated(loggedInUser);
+    }
+  }, []);
+
+  if (!authenticated) {
+    return <Navigate replace to="/login" />;
+  } else {
+    return (
+      <div className="soundboard grid grid-cols-5">
+        <div className="col-start-2 col-span-3 items-center p-4">
+          <h1 className="text-center text-4xl">{me.username}'s Dashboard</h1>
+        </div>
+        <div className="col-start-2 col-span-3 items-center p-4">
+          <h2 className="text-center text-3xl">My Sounds:</h2>
+        </div>
+        <div className="col-start-2 col-span-3 items-center p-2">
+          <ul style={{ listStyleType: 'none' }}>
+            {mySounds.map((data) => (
+              <Sound
+                key={data._id}
+                _id={data._id}
+                url={data.url}
+                soundName={data.soundName}
+                removeSound={removeSound}
+              />
+            ))}
+          </ul>
+        </div>
       </div>
-      <div className="col-start-2 col-span-3 items-center p-4">
-        <h2 className="text-center text-3xl">My Sounds:</h2>
-      </div>
-      <div className="col-start-2 col-span-3 items-center p-2">
-        <ul style={{ listStyleType: 'none' }}>
-          {mySounds.map((data) => (
-            <Sound
-              key={data._id}
-              _id={data._id}
-              url={data.url}
-              soundName={data.soundName}
-              removeSound={removeSound}
-            />
-          ))}
-        </ul>
-      </div>
-    </div>
-  );
+    );
+  }
 };
 
 export default Dashboard;
