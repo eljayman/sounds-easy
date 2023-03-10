7ed14d1e042b183aad413258bca0e9a8f0f23e13
Leland Johnson <112910305+eljayman@users.noreply.github.com>
2023-03-09 18:05:50 -0700

    Update auth.js

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -17,7 +17,7 @@ class AuthService {
   login(idToken) {
     // Saves user token to localStorage and reloads the application for logged in status to take effect
     localStorage.setItem('id_token', idToken);
-    window.location.assign('/dashboard');
+    window.location.replace('/dashboard');
   }
   logout() {
     // Clear user token and profile data from localStorage
