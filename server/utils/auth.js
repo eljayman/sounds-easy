b8588c23be7acec7f5fa8035b819513b1d3c36a8
eljayman <lelandoj@gmail.com>
2023-03-18 07:50:47 -0600

    minor changes

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const secret = process.env.SECRET;
-const expiration = '8h';
+const expiration = '5m';
 
 module.exports = {
   authMiddleware: function ({ req }) {
